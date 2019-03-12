149f9805e85054c4b71e7f393e2e171bdf477629
David Riches <hi@davidrich.es>
2019-03-12 15:30:43 +0000

    adjusting copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ const IndexPage = (props) => (
   <Layout>
     <Section>
       <Heading>This is a <a target="_blank" rel="noopener noreferrer" href="https://www.styled-components.com/"><span className="MainText">Styled Components</span><span className="HoverText">Styled Components</span></a> demo.</Heading>
-      <SubHeading>Built in React, using <a target="_blank" rel="noopener noreferrer" href="https://www.gatsbyjs.org/"><span className="MainText">Gatsby.js</span><span className="HoverText">Gatsby.js</span></a> and hosted on <a target="_blank" rel="noopener noreferrer" href="https://www.netlify.com/"><span className="MainText">Netlify.</span><span className="HoverText">Netlify.</span></a></SubHeading>
-      <p>Read <Link to="/random-facts"><span className="MainText">here</span><span className="HoverText">here</span></Link> some random facts about Darkness.</p>
+      <SubHeading>Why don't you read some random facts about Darkness <Link to="/random-facts"><span className="MainText">here</span><span className="HoverText">here.</span></Link></SubHeading>
+      <p>This site is built in React, using <a target="_blank" rel="noopener noreferrer" href="https://www.gatsbyjs.org/"><span className="MainText">Gatsby.js</span><span className="HoverText">Gatsby.js</span></a><br/> and hosted on <a target="_blank" rel="noopener noreferrer" href="https://www.netlify.com/"><span className="MainText">Netlify.</span><span className="HoverText">Netlify.</span></a></p>
     </Section>
   </Layout>
 )
